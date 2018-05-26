7d8187f31909d5605438266c6070a9d2fe1153b0
jiang zhuolin <jiangzl5@lenovo.com>
2018-05-27 00:37:19 +0800

    添加随机 user-agent 配置，更改休眠时间为半小时

--- proxy_ip/settings.py
+++ proxy_ip/settings.py
@@ -56,6 +56,7 @@ COOKIES_ENABLED = False
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
+    'proxy_ip.middlewares.RandomUserAgentMiddleware': 100,
 }
 
 # Enable or disable extensions
